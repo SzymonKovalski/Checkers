7e70a42702dc23c41dd2d1323e5ba068d14205d5
SzymonKovalski <kovalski.szymon@gmail.com>
2021-06-15 10:53:55 +0300

    Update Checkers.test.js

--- Checkers.test.js
+++ Checkers.test.js
@@ -3,18 +3,18 @@ function calculateCoordinatesFromId(id) {
 	let currentId = 0;
 	for (let i = 0; i < boardLength; i++) {
 		for (let j = 0; j < boardLength; j++) {
-			currentId++;
 			if (currentId === id) {
 				const location = [i, j];
 				return location;
 			}
+            currentId++;
 		}
 	}
 } // i do not know how to import this directly so i copy-paste
 
 
 test('properly finds coordinates', () => {
-    expect(calculateCoordinatesFromId(15)).toBe([1, 7]);
+    expect(calculateCoordinatesFromId(15)).toEqual([1, 7]);
 });
 
 // npm test
