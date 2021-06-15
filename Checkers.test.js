d20dcdc9a43f1612dc910ca83cece80457fd92eb
SzymonKovalski <kovalski.szymon@gmail.com>
2021-06-15 10:50:03 +0300

    Update Checkers.test.js

--- Checkers.test.js
+++ Checkers.test.js
@@ -1,7 +1,20 @@
-const calculateCoordinatesFromId = require('./calculateCoordinatesFromId');
-
+const boardLength = 8;
+function calculateCoordinatesFromId(id) {
+	let currentId = 0;
+	for (let i = 0; i < boardLength; i++) {
+		for (let j = 0; j < boardLength; j++) {
+			currentId++;
+			if (currentId === id) {
+				const location = [i, j];
+				return location;
+			}
+		}
+	}
+} // i do not know how to import this directly so i copy-paste
 
 
 test('properly finds coordinates', () => {
-    expect(calculateCoordinatesFromId(15)).toBe([7], [1]);
+    expect(calculateCoordinatesFromId(15)).toBe([7, 1]);
 });
+
+// npm testnpm test
