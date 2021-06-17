a487e2669dd35e9b479a48ee104b67bb138ca8f7
Zhang Jiabao <z1014438872@gmail.com>
2021-06-17 10:46:44 +0300

    update unit testing

--- Checkers.js
+++ Checkers.js
@@ -6,6 +6,8 @@
 
 
 'use strict';
+
+
 // ||x
 // \/ y->
 const bblack = 2;
@@ -54,7 +56,10 @@ const moveNames = {
 	3: 'rightDown',
 };
 const boardLength = 8;
-
+export {
+	calculateCoordinatesFromId as cCFI,
+	calculateIdFromCoordinates as cIFC
+}
 function createButtons1() {
 	for (let i = 0; i < 64; i++) {
 		const element = document.getElementById(i);
@@ -68,8 +73,8 @@ function createButtons1() {
 }
 function calculateCoordinatesFromId(id) {
 	let currentId = 0;
-	for (let i = 0; i < boardLength; i++) {
-		for (let j = 0; j < boardLength; j++) {
+	for (let i = 0; i < 8; i++) {
+		for (let j = 0; j < 8; j++) {
 			if (currentId === id) {
 				const location = [i, j];
 				return location;
@@ -78,6 +83,7 @@ function calculateCoordinatesFromId(id) {
 		}
 	}
 }
+
 function calculateIdFromCoordinates(coordinates) {
 	const id = 8 * coordinates[0] + coordinates[1];
 	return id;
--- Checkers.test.js
+++ Checkers.test.js
@@ -1,33 +1,12 @@
-const boardLength = 8;
-/*
-function calculateCoordinatesFromId(id) {
-	let currentId = 0;
-	for (let i = 0; i < boardLength; i++) {
-		for (let j = 0; j < boardLength; j++) {
-			if (currentId === id) {
-				const location = [i, j];
-				return location;
-			}
-            currentId++;
-		}
-	}
-} // i do not know how to import this directly so i copy-paste
-
-
-test('properly finds coordinates', () => {
-    expect(calculateCoordinatesFromId(15)).toEqual([1, 7]);
+import {cCFI,cIFC} from './Checkers'
+test('calculateCoordinatesFromId', () => {
+    expect(cCFI(15)).toEqual([1, 7]);
 });
-*/
 
-/*
-function calculateIdFromCoordinates(coordinates) {
-	const id = 8 * coordinates[0] + coordinates[1];
-	return id;
-}
-test('properly finds coordinates', () => {
-    expect(calculateIdFromCoordinates([1,7])).toEqual(15);
+test('calculateIdFromCoordinates', () => {
+    expect(cIFC([1,7])).toEqual(15);
 });
-*/
+
 
 
 // npm test
